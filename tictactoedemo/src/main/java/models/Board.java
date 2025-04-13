117c8230865d39bbdb3554e3878e4e88a3444fe8
anshulv1401 <anshulv1401@gmail.com>
2025-04-13 17:04:04 +0530

    Board created

--- tictactoedemo/src/main/java/models/Board.java
+++ tictactoedemo/src/main/java/models/Board.java
@@ -1,5 +1,21 @@
 package main.java.models;
 
-public class Board {
+import java.util.ArrayList;
+import java.util.List;
 
+public class Board {
+    private List<List<Cell>> board;
+    private int dimenstion;
+
+    public Board(int dimenstion) {
+        this.dimenstion = dimenstion;
+        board = new ArrayList<>();
+
+        for (int i = 0; i < dimenstion; i++) {
+            board.add(new ArrayList<>());
+            for (int j = 0; j < dimenstion; j++) {
+                board.get(i).add(new Cell());
+            }
+        }
+    }
 }
--- tictactoedemo/src/main/java/models/Game.java
+++ tictactoedemo/src/main/java/models/Game.java
@@ -1,5 +1,18 @@
 package main.java.models;
 
-public class Game {
+import java.util.List;
 
+import main.java.enums.GameState;
+
+public class Game {
+    private Board board;
+    private List<Player> players;
+    private List<Move> moves;
+    private Player player;
+    private int nextPlayerTurnIndex;
+    private GameState gameState;
+
+    public Game() {
+
+    }
 }
