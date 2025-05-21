6041ed39c258c2702c8d5ca8233c4d7eab6c930f
anshulv1401 <anshulv1401@gmail.com>
2025-05-22 00:05:43 +0530

    entity listener added

--- bookmyshow/src/main/java/dev/anshul/bookmyshow/models/BaseModel.java
+++ bookmyshow/src/main/java/dev/anshul/bookmyshow/models/BaseModel.java
@@ -1,5 +1,10 @@
 package dev.anshul.bookmyshow.models;
 
+import org.springframework.data.annotation.CreatedDate;
+import org.springframework.data.annotation.LastModifiedDate;
+import org.springframework.data.jpa.domain.support.AuditingEntityListener;
+
+import jakarta.persistence.EntityListeners;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
@@ -10,12 +15,16 @@ import lombok.Setter;
 @Getter
 @Setter
 @MappedSuperclass
+@EntityListeners(AuditingEntityListener.class)
 public class BaseModel {
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
+    @CreatedDate
     private Long createdAt;
+
+    @LastModifiedDate
     private Long lastModifiedAt;
 }
