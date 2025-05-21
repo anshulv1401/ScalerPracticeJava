9391fd9f13a9fe9826edb797b9971039ebc27568
anshulv1401 <anshulv1401@gmail.com>
2025-05-22 00:05:14 +0530

    bean corrected

--- bookmyshow/src/main/java/dev/anshul/bookmyshow/configs/BCryptPasswordEncoderConfig.java
+++ bookmyshow/src/main/java/dev/anshul/bookmyshow/configs/BCryptPasswordEncoderConfig.java
@@ -1,8 +1,10 @@
 package dev.anshul.bookmyshow.configs;
 
 import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 
+@Configuration
 public class BCryptPasswordEncoderConfig {
 
     // Beans are spring objects managed by bean
