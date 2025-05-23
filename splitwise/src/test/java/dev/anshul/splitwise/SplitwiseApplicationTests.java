36f661649875c88085f1ce08227eefbe67338869
anshulv1401 <anshulv1401@gmail.com>
2025-05-24 03:13:08 +0530

    split wise project added

---
+++ splitwise/src/test/java/dev/anshul/splitwise/SplitwiseApplicationTests.java
@@ -0,0 +1,13 @@
+package dev.anshul.splitwise;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SplitwiseApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
